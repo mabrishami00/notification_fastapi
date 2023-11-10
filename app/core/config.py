d9bdd9e7e94b2008daf55cfef3c47ca5e68d5bdf
mabrishami00 <mabrishami78@yahoo.com>
2023-11-10 08:38:08 +0330

    chore: add configs

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,17 @@
 from pydantic_settings import BaseSettings
 
+
 class Settings(BaseSettings):
-    REDIS_URL = 'redis://localhost:6379/2'
+    REDIS_URL: str
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+
+    class Config:
+        env_file = "/home/mahdi/Documents/rss_project/notification_fastapi/.env"
 
 
 settings = Settings()
